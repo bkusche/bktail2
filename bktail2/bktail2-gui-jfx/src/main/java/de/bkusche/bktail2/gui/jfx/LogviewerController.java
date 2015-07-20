183c1eb9f9e87135adba16af3baeb2f313bdb29e
Björn Kusche <prayerb80@googlemail.com>
2015-07-21 00:53:16 +0200

    ADD: introducing logview

---
+++ bktail2/bktail2-gui-jfx/src/main/java/de/bkusche/bktail2/gui/jfx/LogviewerController.java
@@ -0,0 +1,115 @@
+package de.bkusche.bktail2.gui.jfx;
+
+import java.awt.ScrollPane;
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import com.sun.javafx.scene.control.skin.ListViewSkin;
+import com.sun.javafx.scene.control.skin.VirtualFlow;
+
+import de.bkusche.bktail2.logfilehandler.I_LogfileEventListener;
+import de.bkusche.bktail2.logfilehandler.I_LogfileHandler;
+import de.bkusche.bktail2.logfilehandler.LogfileEvent;
+import de.bkusche.bktail2.logfilehandler.LogfileReadInput;
+import de.bkusche.bktail2.logfilehandler.impl.S_LogfileHandlerImpl;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.shape.Rectangle;
+
+@SuppressWarnings({"rawtypes","unchecked"})
+public class LogviewerController implements I_LogfileEventListener, Initializable{
+
+	final String filepath = "/opt/jboss/standalone/log/server.log";
+	final int lines = 80;
+	
+	@FXML ScrollPane scrollPane;
+//	@FXML TextArea logContent;
+	
+	@FXML ListView logContent;
+	
+	private I_LogfileHandler logfileHandler;
+	
+	private ScheduledExecutorService executorService;
+	private int first;
+	private int last;
+	private double rowHight;
+	
+	private Runnable b = () -> {
+		
+	};
+	
+	public LogviewerController() {
+		
+		logfileHandler = S_LogfileHandlerImpl.getInstance();
+		logfileHandler.addFileToWatch((new File( filepath )));
+		logfileHandler.addLogfileEventListener(this);
+		
+		executorService = Executors.newScheduledThreadPool(1);
+	}
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		ObservableList ob = FXCollections.observableList(new ArrayList<>());
+		logContent.setItems(ob);
+		logContent.getItems().add("start");
+		executorService.execute(() ->{
+			while(true){
+				try {
+					ListViewSkin<?> ts = (ListViewSkin<?>) logContent.getSkin();
+			        VirtualFlow<?> vf = (VirtualFlow<?>) ts.getChildren().get(0);
+			        first = vf.getFirstVisibleCell().getIndex();
+			        last = vf.getLastVisibleCell().getIndex();
+			        rowHight = vf.getFirstVisibleCell().getHeight();
+			        //System.out.println("##### Scrolling first "+first+" last "+last);
+					Thread.sleep(100L);
+				} catch (Throwable e) {
+					// TODO: handle exception
+				}	
+			}
+		});
+	}
+
+	@Override
+	public void onCreate(final LogfileEvent event) {
+		Platform.runLater( () -> {
+			logContent.getItems().clear();
+			logfileHandler.readLines(new LogfileReadInput(
+					event.getPath(), 
+					first, 
+					event.getLines() > lines? lines : event.getLines(), 
+					event.getLines())).forEach(l -> logContent.getItems().add(l+"\n"));
+//			//height
+//			logContent.getItems().add("<html><body><div style=\"height:864px;text-align: center;\"></body></html>");
+//			
+			Rectangle r = new Rectangle();
+			r.setHeight((event.getLines()-lines)*rowHight);
+			logContent.getItems().add(r);
+			logContent.getItems().add("end");
+		});
+		
+	}
+
+	@Override
+	public void onModify(LogfileEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onDelete(LogfileEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
--- bktail2/bktail2-gui-jfx/src/main/java/de/bkusche/bktail2/gui/jfx/MainApp.java
+++ bktail2/bktail2-gui-jfx/src/main/java/de/bkusche/bktail2/gui/jfx/MainApp.java
@@ -20,16 +20,16 @@ public class MainApp extends Application {
 
         log.info("Starting Hello JavaFX and Maven demonstration application");
 
-        String fxmlFile = "/fxml/hello.fxml";
+        String fxmlFile = "/fxml/Logviewer.fxml";
         log.debug("Loading FXML for main view from: {}", fxmlFile);
         FXMLLoader loader = new FXMLLoader();
         Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
 
         log.debug("Showing JFX scene");
-        Scene scene = new Scene(rootNode, 400, 200);
+        Scene scene = new Scene(rootNode, 1024, 768);
         scene.getStylesheets().add("/styles/styles.css");
 
-        stage.setTitle("Hello JavaFX and Maven");
+        stage.setTitle("Logviewer");
         stage.setScene(scene);
         stage.show();
     }
