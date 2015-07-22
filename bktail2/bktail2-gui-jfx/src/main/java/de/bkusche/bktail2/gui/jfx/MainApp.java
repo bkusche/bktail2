2701ae09128e427397d8a0d34cf2ae58a3ce4ca5
Björn Kusche <prayerb80@googlemail.com>
2015-07-22 22:52:10 +0200

    MOD: get rid of the unused logging frameworks slf4j & log4j

--- bktail2/bktail2-gui-jfx/src/main/java/de/bkusche/bktail2/gui/jfx/MainApp.java
+++ bktail2/bktail2-gui-jfx/src/main/java/de/bkusche/bktail2/gui/jfx/MainApp.java
@@ -5,32 +5,24 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class MainApp extends Application {
 
-    private static final Logger log = LoggerFactory.getLogger(MainApp.class);
+	public static void main(String[] args) throws Exception {
+		launch(args);
+	}
 
-    public static void main(String[] args) throws Exception {
-        launch(args);
-    }
+	public void start(Stage stage) throws Exception {
 
-    public void start(Stage stage) throws Exception {
+		String fxmlFile = "/fxml/Logviewer.fxml";
 
-        log.info("Starting Hello JavaFX and Maven demonstration application");
+		FXMLLoader loader = new FXMLLoader();
+		Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
+		Scene scene = new Scene(rootNode, 1024, 768);
 
-        String fxmlFile = "/fxml/Logviewer.fxml";
-        log.debug("Loading FXML for main view from: {}", fxmlFile);
-        FXMLLoader loader = new FXMLLoader();
-        Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
-
-        log.debug("Showing JFX scene");
-        Scene scene = new Scene(rootNode, 1024, 768);
-        scene.getStylesheets().add("/styles/styles.css");
-
-        stage.setTitle("Logviewer");
-        stage.setScene(scene);
-        stage.show();
-    }
+		scene.getStylesheets().add("/styles/styles.css");
+		stage.setTitle("Logviewer");
+		stage.setScene(scene);
+		stage.show();
+	}
 }
