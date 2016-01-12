e2b10b796b73d88d506a0cfa3e06b5bec2a6e855
bkusche <bkdevelopergit@gmail.com>
2016-01-12 01:34:41 +0100

    ADD: License file MOD: added license headers

--- bktail2/bktail2-gui-jfx/src/main/java/de/bkusche/bktail2/gui/jfx/HighlightingController.java
+++ bktail2/bktail2-gui-jfx/src/main/java/de/bkusche/bktail2/gui/jfx/HighlightingController.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2016 Björn Kusche
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.bkusche.bktail2.gui.jfx;
 
 import java.util.prefs.BackingStoreException;
@@ -27,6 +42,10 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.util.converter.DefaultStringConverter;
 
+/**
+ * @author bkusche
+ *
+ */
 public class HighlightingController {
 
     @FXML Label lblForBackColorTitle;
