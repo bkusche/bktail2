2ae9706983f31c87591cd79c2928c624d5512ba3
bkusche <bkdevelopergit@gmail.com>
2017-08-08 22:26:37 +0200

    MOD: use custom jre

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
 #java --module-path bktail2-logfile-handler/target/bktail2-logfile-handler-0.0.1-SNAPSHOT.jar:bktail2-gui-jfx/target/bktail2-gui-jfx.jar -m de.bkusche.bktail2.gui.jfx/de.bkusche.bktail2.gui.jfx.MainApp
-java --module-path bktail2-gui-jfx/target/bktail2-gui-jfx.jar:bktail2-logfile-handler/target/bktail2-logfile-handler-0.0.1-SNAPSHOT.jar -m de.bkusche.bktail2.gui.jfx/de.bkusche.bktail2.gui.jfx.MainApp
+
+BKTAIL2_JRE="target/bktail2_jre"
+
+if [ -d "$BKTAIL2_JRE" ]; then
+    JAVA_HOME=$BKTAIL2_JRE
+fi
+
+echo "using JVM: $JAVA_HOME"
+$JAVA_HOME/bin/java --module-path bktail2-gui-jfx/target/bktail2-gui-jfx.jar:bktail2-logfile-handler/target/bktail2-logfile-handler-0.0.1-SNAPSHOT.jar -m de.bkusche.bktail2.gui.jfx/de.bkusche.bktail2.gui.jfx.MainApp
