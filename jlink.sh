0ffab3cb696f47692441c19821cbda05b21629b4
Björn Kusche <bjoern.kusche@openlimit.com>
2017-08-10 13:51:23 +0200

    MOD: remove assembly plugin / script

--- jlink.sh
+++ jlink.sh
@@ -2,7 +2,7 @@
 JLINK=${JAVA_HOME}/bin/jlink
 JMODS=${JAVA_HOME}/jmods
 
-${JLINK} --module-path $JMODS:bktail2-gui-jfx/target/bktail2-gui-jfx.jar:bktail2-logfile-handler/target/bktail2-logfile-handler-0.0.1-SNAPSHOT.jar --no-header-files --no-man-pages -G --compress=1 --add-modules=de.bkusche.bktail2.gui.jfx --limit-modules de.bkusche.bktail2.gui.jfx --output=target/bktail2_jre 
+${JLINK} --module-path $JMODS:bktail2-gui-jfx/target/bktail2-gui-jfx-0.0.1-SNAPSHOT.jar:bktail2-logfile-handler/target/bktail2-logfile-handler-0.0.1-SNAPSHOT.jar --strip-debug --no-header-files --no-man-pages -G --compress=1 --add-modules=de.bkusche.bktail2.gui.jfx --limit-modules de.bkusche.bktail2.gui.jfx --output=target/bktail2_jre 
 
 #JVMMODS=$JMODS:java.base.jmod:$JMODS:java.prefs.jmod:$JMODS:javafx.base.jmod:$JMODS:javafx.controls.jmod:$JMODS:javafx.fxml.jmod:$JMODS:javafx.graphics.jmod
 #${JLINK} --module-path $JMODS:bktail2-gui-jfx/target/bktail2-gui-jfx.jar:bktail2-logfile-handler/target/bktail2-logfile-handler-0.0.1-SNAPSHOT.jar --no-header-files --no-man-pages -G --compress=1 --add-modules=de.bkusche.bktail2.gui.jfx --limit-modules de.bkusche.bktail2.gui.jfx --output=target/bktail2_jre 
--- run.sh
+++ run.sh
@@ -9,4 +9,4 @@ if [ -d "$BKTAIL2_JRE" ]; then
 fi
 
 echo "using JVM: $JAVA_HOME"
-$JAVA_HOME/bin/java --module-path bktail2-gui-jfx/target/bktail2-gui-jfx.jar:bktail2-logfile-handler/target/bktail2-logfile-handler-0.0.1-SNAPSHOT.jar -m de.bkusche.bktail2.gui.jfx/de.bkusche.bktail2.gui.jfx.MainApp
+$JAVA_HOME/bin/java --module-path bktail2-gui-jfx/target/bktail2-gui-jfx-0.0.1-SNAPSHOT.jar:bktail2-logfile-handler/target/bktail2-logfile-handler-0.0.1-SNAPSHOT.jar -m de.bkusche.bktail2.gui.jfx/de.bkusche.bktail2.gui.jfx.MainApp
